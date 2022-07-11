16c8d33436839731a5dbb306bc8be60913009b8e
Muhammad Alif Rhamadhan <50443591+malifrh@users.noreply.github.com>
2022-07-11 18:56:48 +0700

    First Commit

---
+++ application/views/templates_admin/sidebar.php
@@ -0,0 +1,107 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+        <img src="<?= base_url() ?>assets/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">AdminLTE 3</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+                <li class="nav-item">
+                    <a href="<?= site_url('admin/dashboard') ?>" class="nav-link">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Dashboard
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="<?= site_url('admin/product') ?>" class="nav-link">
+                        <i class="nav-icon fas fa-gamepad"></i>
+                        <p>
+                            Surat Masuk
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="<?= site_url('admin/history') ?>" class="nav-link">
+                        <i class="nav-icon fas fa-file-invoice"></i>
+                        <p>
+                            Surat Keluar
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="<?= site_url('admin/history') ?>" class="nav-link">
+                        <i class="nav-icon fas fa-file-invoice"></i>
+                        <p>
+                            Disposisi
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="<?= site_url('admin/history') ?>" class="nav-link">
+                        <i class="nav-icon fas fa-file-invoice"></i>
+                        <p>
+                            Agenda
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="<?= site_url('admin/users') ?>" class="nav-link">
+                        <i class="nav-icon fas fa-user"></i>
+                        <p>
+                            Users
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="<?= site_url('admin/users') ?>" class="nav-link">
+                        <i class="nav-icon fas fa-user"></i>
+                        <p>
+                            Master Divisi
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="<?= site_url('admin/users') ?>" class="nav-link">
+                        <i class="nav-icon fas fa-user"></i>
+                        <p>
+                            Master Jabatan
+                        </p>
+                    </a>
+                </li>
+
+                <!-- Nav Item - User Information -->
+                <?php
+                if ($this->session->userdata('email')) {
+                ?>
+                    <li class="nav-item">
+                        <a href="<?= site_url('auth/logout') ?>" class="nav-link">
+                            <i class="nav-icon fa fa-power-off"></i>
+                            <p>
+                                Logout
+                            </p>
+                        </a>
+                    </li>
+                <?php } ?>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
